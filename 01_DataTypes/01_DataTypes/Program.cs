9c351ed895a667894fa801ce176be82601e71c59
Tony Minott <minottanthonya@gmail.com>
2018-08-24 16:48:02 -0400

    added modulus section

--- 01_DataTypes/01_DataTypes/Program.cs
+++ 01_DataTypes/01_DataTypes/Program.cs
@@ -125,7 +125,14 @@ namespace _01_DataTypes
             Console.WriteLine(2 * 3); //result = 6
             Console.WriteLine(10 / 2); //result = 5
 
-            // Integers 
+            // Modulus 
+
+            Console.WriteLine(10 % 2) //result = 0 (10/2 = 5 w/ remainder of 0)
+            Console.WriteLine(10 % 9) //result = 1 (10/9 = 1 w/ remainder of 1)
+            Console.WriteLine(16 % 3) //result = 1 (16/3 = 5 w/ remainder of 1)
+            Console.WriteLine(19 % 5) //result = 4 (19/5 = 3 w/ remainder of 4)
+
+
         }
     }
 }
